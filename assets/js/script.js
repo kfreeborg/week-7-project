8d4af2019dd36131d003dd7e7f6aa8a8bf02034c
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2020-11-09 19:38:55 -0700

    search country added

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,22 +17,13 @@ function summaryData() {
 }
 
 function displaySummary(json) {
-    newCasesEl.textContent = json.Global.NewConfirmed.toLocaleString();
-    totalCasesEl.textContent = json.Global.TotalConfirmed.toLocaleString();
-    newDeathsEl.textContent = json.Global.NewDeaths.toLocaleString();
-    totalDeathsEl.textContent = json.Global.TotalDeaths.toLocaleString();
+  newCasesEl.textContent = json.Global.NewConfirmed.toLocaleString();
+  totalCasesEl.textContent = json.Global.TotalConfirmed.toLocaleString();
+  newDeathsEl.textContent = json.Global.NewDeaths.toLocaleString();
+  totalDeathsEl.textContent = json.Global.TotalDeaths.toLocaleString();
 }
 
 summaryData();
-
-let map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 0, lng: 0 },
-    zoom: 1.5,
-  });
-}
-
 
 let dropdown = $("#search");
 
@@ -43,6 +34,47 @@ dropdown.prop("selectedIndex", 0);
 
 $.getJSON(apiSummary, function (data) {
   $.each(data.Countries, function (key, entry) {
-    dropdown.append($("<option></option>").text(entry.Country));
+    dropdown.append($("<option></option>").text(entry.Country).data(entry));
   })
 });
+
+$("#search").change(function () {
+  var countryData = $(document.getElementById("search").selectedOptions[0]).data();
+
+  console.log(countryData);
+
+  var zoomCountry = $(document.getElementById("search").selectedOptions[0]).data().Country;
+  console.log(zoomCountry);
+  initMap();
+
+});
+
+let map;
+var geocoder;
+
+function initMap() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(0, 0);
+  var mapOptions = {
+    zoom: 1.5,
+    center: latlng
+  }
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
+function codeAddress() {
+  var address = document.getElementById('search').value;
+  var geocoder = new google.maps.Geocoder();
+  console.log(address);
+  geocoder.geocode({ 'address': address }, function (results, status) {
+    if (status == 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
